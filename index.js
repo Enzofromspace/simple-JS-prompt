b610aee4b31fe5703ef3c62f160ae3e0f234915c
Enzofromspace <8026658+Enzofromspace@users.noreply.github.com>
2022-12-21 13:16:29 +0300

    Got cancel button to display - console logs text when submit button is hit

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
 // Import stylesheets
 import './style.css';
+function wait(ms = 0){
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 
 // Write Javascript code!
 function ask(options){
-  return new Promise(function() {
+  return new Promise(async function(resolve) {
     //create a pop-up with prompt fields
     const popup = document.createElement('form');
     popup.classList.add('popup');
-    popup.insertAdjacentHTML('afterbegin', `
-      <fieldset>
+    popup.insertAdjacentHTML('afterbegin', `<fieldset>
          <label>${options.title}</label>
+         <input type="text name="input"/>
+         <button type="submit">Submit</button>
       </fieldset>
     `);
-    
-    console.log(popup);
     //check for cancel if needed
     if(options.cancel){
       const skipButton = document.createElement('button');
       skipButton.type = 'button';
       skipButton.textContent = 'Cancel?';
+      popup.firstElementChild.append(skipButton);
       //listen for a click on cancel button
     }
     //listen for submit
-
+    popup.addEventListener('submit', function(e){
+      e.preventDefault();
+      console.log('submitted');
+    });
     //on submit, resolved the data to inputs 
 
     //insert the popup into the DOM
     document.body.appendChild(popup);
     // use small timeout to avoid display of popup on start
-    setTimeout(function(){
-        popup.classList.add('open');
-      }, 10);
+    await wait(50);
+    popup.classList.add('open');
   });
 }
 
-console.log(ask({title: "works?"}));
+console.log(ask({title: "works?", cancel: true}));
